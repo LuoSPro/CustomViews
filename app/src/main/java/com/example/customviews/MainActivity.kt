2bc069ec78492e4a558c34b71219c325802f9d1e
ls <1584677103@qq.com>
2020-10-12 14:54:03 +0800

    动态加载框效果

--- app/src/main/java/com/example/customviews/MainActivity.kt
+++ app/src/main/java/com/example/customviews/MainActivity.kt
@@ -12,24 +12,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initQQStepView()
+        initDynamicProgress()
     }
 
-    private fun initQQStepView() {
-        step_view.apply {
-            setStepMax(4000)
-        }
-
+    private fun initDynamicProgress() {
         //属性动画
-        val valueAnimator = ObjectAnimator.ofFloat(0f, 3000f)
+        val valueAnimator = ObjectAnimator.ofFloat(0f, 100f)
         valueAnimator.apply {
-            duration = 1000
+            duration = 3000
             interpolator = DecelerateInterpolator()
             addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
                 override fun onAnimationUpdate(animation: ValueAnimator?) {
                     //这里只能先转成Float，直接先转成Int会崩掉
                     val curStep: Float = animation?.animatedValue as Float
-                    step_view.setCurStep(curStep.toInt())
+                    progress_view.setProgress(curStep.toInt())
                 }
             })
             start()
