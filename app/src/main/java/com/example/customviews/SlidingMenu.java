8bc3619b2efee27153a0791beee0c63b64fa6a9f
ls <1584677103@qq.com>
2020-10-18 16:16:06 +0800

    初始化

---
+++ app/src/main/java/com/example/customviews/SlidingMenu.java
@@ -0,0 +1,19 @@
+package com.example.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.HorizontalScrollView;
+
+public class SlidingMenu extends HorizontalScrollView {
+    public SlidingMenu(Context context) {
+        super(context);
+    }
+
+    public SlidingMenu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SlidingMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
