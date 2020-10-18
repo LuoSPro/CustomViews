dac6a4427cde578be4c752256c8f53d3fd266a22
ls <1584677103@qq.com>
2020-10-18 12:36:07 +0800

    内容修改

--- app/src/main/java/com/example/customviews/MainActivity.kt
+++ app/src/main/java/com/example/customviews/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.customviews
 
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
-import android.view.animation.DecelerateInterpolator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
