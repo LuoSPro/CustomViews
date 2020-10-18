56e8d6809f9458e45025185764ff5f119ff3a8fb
ls <1584677103@qq.com>
2020-10-18 12:46:52 +0800

    内容修改

--- app/src/main/java/com/example/customviews/MainActivity.kt
+++ app/src/main/java/com/example/customviews/MainActivity.kt
@@ -14,23 +14,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mHandle = Handler(Looper.getMainLooper())
-        initLoadingView58()
+        initDynamicProgress()
     }
 
-    private fun initLoadingView58() {
-        changeLoadingStatus(LoadingView58.Status.CIRCLE,0);
-        changeLoadingStatus(LoadingView58.Status.TRIANGLE,1000);
-        changeLoadingStatus(LoadingView58.Status.RECTANGLE,2000);
-    }
-
-    private lateinit var mHandle: Handler
-
-    private fun changeLoadingStatus(status: LoadingView58.Status, time: Long ) {
-        mHandle.postDelayed(object : Runnable{
-            override fun run() {
-                loading_view.setStatus(status)
-            }
-        },time)
+    private fun initDynamicProgress() {
+        //属性动画
+        val valueAnimator = ObjectAnimator.ofFloat(0f, 100f)
+        valueAnimator.apply {
+            duration = 3000
+            interpolator = DecelerateInterpolator()
+            addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
+                override fun onAnimationUpdate(animation: ValueAnimator?) {
+                    //这里只能先转成Float，直接先转成Int会崩掉
+                    val curStep: Float = animation?.animatedValue as Float
+                    progress_view.setProgress(curStep.toInt())
+                }
+            })
+            start()
+        }
     }
 }
